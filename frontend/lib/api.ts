d21bd5537a384a1f8fda5477515eeab08811c8e0
Ushan Bartaula <ushanbartaula1994@gmail.com>
2026-05-25 15:05:24 +0545

    change some  code

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://nesxtjs-and-express-blog-app-production.up.railway.app",
   withCredentials: true,
 });
 
