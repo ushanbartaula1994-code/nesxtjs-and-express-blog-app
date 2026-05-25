2c1ffc8b1a079dd0a7743022548452dfdeff5d65
Ushan Bartaula <ushanbartaula1994@gmail.com>
2026-05-25 14:23:14 +0545

    fix cors preflight crash for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import uploadRoutes from "./routes/upload.routes.js";
 
 const app = express();
 
-// 1. CORS
+// 1. CORS 
 app.use(
   cors({
     origin: "https://nesxtjs-and-express-blog-app.vercel.app",
@@ -18,8 +18,8 @@ app.use(
   }),
 );
 
-// 2. Handle preflight requests explicitly (IMPORTANT)
-app.options("*", cors());
+
+
 
 // 3. Body parsers
 app.use(express.json({ limit: "16kb" }));
@@ -32,5 +32,13 @@ app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/upload", uploadRoutes);
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
 
 export default app;
