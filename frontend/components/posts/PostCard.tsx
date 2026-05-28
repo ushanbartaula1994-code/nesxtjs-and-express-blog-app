b436226ba0f68a47fd77ca4c8349bcd1f379a352
Ushan Bartaula <ushanbartaula1994@gmail.com>
2026-05-28 20:26:03 +0545

    added dynamic routing

--- frontend/components/posts/PostCard.tsx
+++ frontend/components/posts/PostCard.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
-import PostActions from "@/components/posts/postActions";
+import dynamic from "next/dynamic";
 import type { Post } from "@/types/types";
+
+const PostActions = dynamic(() => import("@/components/posts/postActions"), {
+  ssr: false,
+});
 
 type Props = {
   post: Post;
@@ -8,15 +12,18 @@ type Props = {
 
 export default function PostCard({ post }: Props) {
   console.log("POST CARD ID:", post._id, typeof post._id);
+
   return (
     <article className="rounded-xl bg-white p-4 border">
       <h2>{post.title}</h2>
+
       {post.image && (
         <Image src={post.image} alt={post.title} width={600} height={300} />
       )}
+
       <p>{post.content}</p>
+
       <PostActions postId={post._id} />
-      
     </article>
   );
 }
