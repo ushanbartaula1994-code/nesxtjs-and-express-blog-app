d470dc1df09f849f9bae7d9a84f6c1034caed701
Ushan Bartaula <ushanbartaula1994@gmail.com>
2026-05-29 14:36:07 +0545

    change to server component

--- frontend/components/posts/PostCard.tsx
+++ frontend/components/posts/PostCard.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
 import PostActions from "@/components/posts/postActions";
 import type { Post } from "@/types/types";
-
+import  PostCardLink from "@/components/posts/PostCardLink";
 type Props = {
   post: Post;
 };
 
 export default function PostCard({ post }: Props) {
   return (
-    <article className="rounded-xl bg-white p-4 border">
-      <h2>{post.title}</h2>
+    <PostCardLink postId={post._id}>
+      <article className="rounded-xl bg-white p-4 border">
+        <h2>{post.title}</h2>
 
-      {post.image && (
-        <Image src={post.image} alt={post.title} width={600} height={300} />
-      )}
+        {post.image && (
+          <Image src={post.image} alt={post.title} width={600} height={300} />
+        )}
 
-      <p>{post.content}</p>
+        <p>{post.content}</p>
 
-      <PostActions postId={post._id} />
-    </article>
+        <PostActions postId={post._id} />
+      </article>
+    </PostCardLink>
   );
 }
---
+++ frontend/components/posts/PostCardLink.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  postId: string;
+};
+
+export default function PostCardLink({ children, postId }: Props) {
+  const router = useRouter();
+
+  return (
+    <div
+      onDoubleClick={() => router.push(`/posts/${postId}`)}
+      className="cursor-pointer"
+    >
+      {children}
+    </div>
+  );
+}
