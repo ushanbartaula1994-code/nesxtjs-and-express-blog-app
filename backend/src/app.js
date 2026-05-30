68f7e93be1bfb602a2569fa54ac89e00485e679e
Ushan Bartaula <ushanbartaula1994@gmail.com>
2026-05-30 22:58:09 +0545

    fixing image

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use(
 // 3. Body parsers
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use("/uploads", express.static("uploads"));
+
 // 4. Cookies
 app.use(cookieParser());
 
--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,11 +1,10 @@
 import { Post } from "../models/post.model.js";
+import cloudinary from "../config/cloudinary.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-
 // CREATE POST
-
 export const createPost = asyncHandler(async (req, res) => {
   const { title, content } = req.body;
 
@@ -13,10 +12,23 @@ export const createPost = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Title and content are required");
   }
 
+  let imageUrl = null;
+
+  if (req.file) {
+    const base64 = req.file.buffer.toString("base64");
+    const dataURI = `data:${req.file.mimetype};base64,${base64}`;
+
+    const result = await cloudinary.uploader.upload(dataURI, {
+      folder: "blog-images",
+    });
+
+    imageUrl = result.secure_url;
+  }
+
   const post = await Post.create({
     title,
     content,
-  image: req.file ? `/uploads/${req.file.filename}` : null, // multer file handling
+    image: imageUrl,
     author: req.user._id,
   });
 
@@ -24,6 +36,7 @@ export const createPost = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(201, post, "Post created successfully"));
 });
+
 // GET ALL POSTS
 export const getAllPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find()
@@ -34,11 +47,12 @@ export const getAllPosts = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, posts, "Posts fetched successfully"));
 });
+
 // GET SINGLE POST
 export const getPostsById = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
-  const post = await Post.findById(id).populate("author", "username email")
+  const post = await Post.findById(id).populate("author", "username email");
 
   if (!post) {
     throw new ApiError(404, "Post not found");
@@ -46,12 +60,11 @@ export const getPostsById = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(new ApiResponse(200, post, "Post fetched successfully"))
-})
+    .json(new ApiResponse(200, post, "Post fetched successfully"));
+});
 
 // UPDATE POST
 export const updatePost = asyncHandler(async (req, res) => {
-  console.log("REQ FILE:", req.file);
   const { id } = req.params;
   const { title, content } = req.body;
 
@@ -65,14 +78,20 @@ export const updatePost = asyncHandler(async (req, res) => {
     throw new ApiError(403, "You are not allowed to perform this action");
   }
 
-  // update fields safely
   if (title !== undefined) post.title = title;
   if (content !== undefined) post.content = content;
 
-  // multer file handling
- if (req.file) {
-   post.image = `/uploads/${req.file.filename}`;
- }
+  // CLOUDINARY IMAGE UPDATE
+  if (req.file) {
+    const base64 = req.file.buffer.toString("base64");
+    const dataURI = `data:${req.file.mimetype};base64,${base64}`;
+
+    const result = await cloudinary.uploader.upload(dataURI, {
+      folder: "blog-images",
+    });
+
+    post.image = result.secure_url;
+  }
 
   await post.save();
 
