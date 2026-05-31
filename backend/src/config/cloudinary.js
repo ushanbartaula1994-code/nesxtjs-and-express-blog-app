bf3f61b98a8e4d05c10aac3ed6d587bf09964c5d
Ushan Bartaula <ushanbartaula1994@gmail.com>
2026-05-31 11:00:44 +0545

    cloudinary check

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -5,5 +5,9 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
+console.log("CLOUDINARY CHECK:", {
+  cloud: process.env.CLOUDINARY_CLOUD_NAME,
+  key: process.env.CLOUDINARY_API_KEY,
+  secret: process.env.CLOUDINARY_API_SECRET?.slice(0, 4),
+});
 export default cloudinary;
